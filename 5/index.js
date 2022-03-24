e7107b702f44504e17e74cdb80f3a36ad8d4553c
Ela Piech <e-l-a-p@wp.pl>
2022-03-24 20:28:11 +0100

    Add code with solutions for problems in 5 exercise

--- 5/index.js
+++ 5/index.js
@@ -1,28 +1,15 @@
-function fetchData(params, onSuccess, onError) {
-    const req = new XMLHttpRequest();
-    req.open(params.method, params.url, true);
-    req.onreadystatechange = function (aEvt) {
-        if (req.readyState === 4) {
-            if(req.status === 200) {
-                onSuccess(JSON.parse(req.responseText))
-            } else {
-                onError()
-            }
-        }
-    };
-    req.send(null);
+async function fetchProfile(userId) {
+    const res = await fetch(`http://localhost:8080/app/profile/${userId}`);
+    return res.json();
 }
 
-function fetchProfile(userId, onSuccess, onError) {
-    fetchData({ method: 'GET', url: `http://localhost:8080/app/profile/${userId}`}, onSuccess, onError)
+async function fetchPaymentDetails(userId) {
+   const res = await fetch(`http://localhost:8080/app/payments/${userId}`);
+   return res.json();
 }
 
-function fetchPaymentDetails(userId, onSuccess, onError) {
-    fetchData({ method: 'GET', url: `http://localhost:8080/app/payments/${userId}`}, onSuccess, onError)
-}
-
-var userProfile = null
-var userId = 4
+let userProfile = null
+let userId = 4
 
 fetchProfile(userId, (profile) => {
     userProfile = profile
@@ -30,9 +17,16 @@ fetchProfile(userId, (profile) => {
     window.alert('Cannot fetch profile!')
 })
 
-fetchPaymentDetails(userId, (payments) => {
+try{
+ (async ()=>{
+
+    const [userProfile, payments] = await Promise.all([fetchProfile(userId),
+    fetchPaymentDetails(userId)]) //returns values when all promoses are resolved.
+
+
     document.querySelector('#user-name').textContent = `User: ${userProfile.firstName} ${userProfile.lastName}`
     document.querySelector('#user-subscription').textContent = `Subscription: ${payments.subscriptionStatus}`
-}, () => {
+    })()}
+    catch{
     window.alert('Cannot fetch payment details!')
-})
+    }
